13854207454aa0cefff2faf174e79815c863dfa3
fasoto <fsoto@trainsolutions.com.ar>
2018-09-28 14:27:42 -0300

    Agregando ejemplo con cli

---
+++ zt-ex-cli/src/components/Menu.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="hello">
+    <b-navbar toggleable="md" type="dark" variant="info">
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+      <b-navbar-brand href="#" v-show="logoImg">
+        <img title="logoStr" src="logoImg"></img>
+      </b-navbar-brand>
+      <b-navbar-brand href="#" v-show="!logoImg">{{logoStr}}</b-navbar-brand>
+
+      <b-collapse is-nav id="nav_collapse">
+
+        <b-navbar-nav v-for="item in menu" :key="item.id">
+          <b-nav-item :to="item.url">{{item.label}}</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item-dropdown right>
+            <!-- Using button-content slot -->
+            <template slot="button-content">
+              <em>{{username}}</em>
+            </template>
+            <b-dropdown-item v-for="itemU in menuUser" :key="itemU.id" :to="itemU.url">
+              {{itemU.label}}
+            </b-dropdown-item>
+            <b-dropdown-item @click="logout()">Salir</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+// import menu from '../apiClients/menu's
+import _ from 'lodash'
+
+var menu = [
+  {label:'Link 1', id:'', url: ''},
+  {label:'About', id:'', url: '/about'},
+]
+
+export default {
+  name: 'Menu',
+  data () {
+    return {
+      username: 'Pepito',
+      menu: [],
+      menuUser: [],
+      logoStr: 'App EX',
+      logoImg: ''
+    }
+  },
+  created () {
+    this.menu = menu
+    // menu.get()
+    //   .then((result) => {
+    //     if (result.status === 200) {
+    //       let actions = JSON.parse(result.data.result)
+    //       this.menu = _.filter(actions, (item) => {
+    //         return item.level === 1 && item.menu
+    //       })
+
+    //       this.menuUser = _.filter(actions, (item) => {
+    //         return item.level === 2 & item.menu
+    //       })
+    //     } else {
+    //       this.logout()
+    //     }
+    //   }).catch((error) => {
+    //     if (error.response) {
+    //       this.logout()
+    //     }
+    //   })
+
+    // auth.getUser()
+    //   .then((result) => {
+    //     if (result.status === 200) {
+    //       let userData = JSON.parse(result.data.result)
+    //       if (userData.username) {
+    //         this.username = userData.username
+    //       }
+    //     } else {
+    //       this.logout()
+    //     }
+    //   }).catch(() => {
+    //     this.logout()
+    //     // if (error.status == 409) {
+    //     // }
+    //   })
+
+    // implementation.get()
+    //   .then((result) => {
+    //     if (result.status === 200) {
+    //       let implData = JSON.parse(result.data.message)
+    //       if (implData.logo) {
+    //         this.logoImg = implData.logo
+    //         this.logoStr = implData.name
+    //       }
+    //     } else {
+    //       this.logout()
+    //     }
+    //   }).catch(() => {
+    //     this.logout()
+    //   })
+  },
+  methods: {
+    logout () {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ zt-ex-cli/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+  	<Menu/>
+  	<Layout/>
+  	<Footer/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <!-- <Home msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Layout from '@/components/Layout.vue';
+import Menu from '@/components/Menu.vue';
+import Footer from '@/components/Footer.vue';
+
+export default {
+  name: 'home',
+  components: {
+    Menu,
+    Layout,
+    Footer,
+  },
+};
+</script>
